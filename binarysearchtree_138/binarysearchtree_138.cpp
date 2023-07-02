97e289c53aab6a70765a3acc058dd831d0f2e266
NilaAyu1611 <nilaayucw@gmail.com>
2023-07-02 18:57:53 +0700

    pada method search ini kita menggunkaan bantuan currentnode (ROOT = START)

--- binarysearchtree_138/binarysearchtree_138.cpp
+++ binarysearchtree_138/binarysearchtree_138.cpp
@@ -44,7 +44,7 @@ public:
 			ROOT = newNode;		// Langkah 5.a. Mark the new node as ROOT
 			return;				// Langkah 5.b Exit
 		}
-		
+
 		if (element < parent->info)		// Langkah 6. If the value in the data field of the new node is greater than of the parent
 		{
 			parent->leftchild = newNode;	// Langkah 6.a. Make the left child of the parent point to the new node
@@ -53,4 +53,12 @@ public:
 		{
 			parent->rightchild = newNode;	// 7.a. Make the right child of the parent point to the new node
 		}
+	}
+
+	void search(string element, Node*& parent, Node*& currentNode)
+	{
+		
+	}
+
+
 };
