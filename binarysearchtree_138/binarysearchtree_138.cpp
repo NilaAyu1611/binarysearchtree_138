51ec9c033bb208efbdc56a8fb68e32ef0487192f
NilaAyu1611 <nilaayucw@gmail.com>
2023-07-02 17:40:38 +0700

    langkah 2 ini masukan nilai  pada node baru

--- binarysearchtree_138/binarysearchtree_138.cpp
+++ binarysearchtree_138/binarysearchtree_138.cpp
@@ -31,5 +31,6 @@ public:
 	void insert(string element)		// Insert a node in the binary search tree
 	{
 		Node* newNode = new Node(element, NULL, NULL);		// Langkah 1. Allocate memory for the new node
+		newNode->info = element;		// Langkah 2. Assign value to the data field of the new node
 	}
 };
